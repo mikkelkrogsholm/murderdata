88111ff7dec63086855688301d2f6939e40d3f25
Mikkel Freltoft Krogsholm <mikkel@56n.dk>
2017-12-18 16:01:58 +0000

    Updated vignette

--- vignettes/hunting_for_serial_killers.R
+++ vignettes/hunting_for_serial_killers.R
@@ -3,25 +3,6 @@ library(murderdata)
 library(dplyr)
 library(magrittr)
 
-## ----get_data, message=FALSE, warning=FALSE------------------------------
-murder_data <- get_murderdata("supplementary")
-
-## ----serial_killers------------------------------------------------------
-serial_killers <- murder_data %>%
-  mutate(Solved = Solved %>% factor() %>% as.numeric() %>% subtract(1)) %>%
-  group_by(VicSex, CNTYFIPS, Weapon) %>%
-  summarise(total = n(),
-            solved = sum(Solved),
-            percentage = mean(Solved)) %>%
-  ungroup() 
-
-## ----possible_serial_killers---------------------------------------------
-possible_serial_killers <- serial_killers %>%
-  filter(VicSex == "Female",
-         percentage <= .33) %>%
-  mutate(unsolved = total - solved) %>%
-  arrange(desc(unsolved))
-
-## ----summary-------------------------------------------------------------
-possible_serial_killers %>% select(-VicSex) %>% head() %>% knitr::kable()
+## ----look_data, message=FALSE, warning=FALSE-----------------------------
+glimpse(md_supplementary)
 
